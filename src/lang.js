f20215285a2ce148351a095c02c64a60d2165a86
Mauricio Estrada Celis <kguttas@hotmail.com>
2022-06-03 23:28:13 -0400

    Se agrega frontend con template Facit

---
+++ src/lang.js
@@ -0,0 +1,33 @@
+const LANG = {
+	EN: {
+		text: 'English',
+		lng: 'en-US',
+		icon: 'CustomUsa',
+	},
+	DE: {
+		text: 'Deutsche',
+		lng: 'de-DE',
+		icon: 'CustomGermany',
+	},
+	FR: {
+		text: 'Français',
+		lng: 'fr-FR',
+		icon: 'CustomFrance',
+	},
+	TR: {
+		text: 'Türkçe',
+		lng: 'tr-TR',
+		icon: 'CustomTurkey',
+	},
+	CL: {
+		text: 'Chile',
+		lng: 'es-CL',
+		icon: 'CustomChile',
+	},
+};
+
+export const getLangWithKey = (key) => {
+	return LANG[Object.keys(LANG).filter((f) => LANG[f].lng === key)];
+};
+
+export default LANG;
